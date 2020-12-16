d6eaf718892b50e37f9a462d15ee0bb895cc7fd7
Elias Ellison <eellison@devfair044.maas>
2020-12-16 10:55:08 -0800

    Add fusion support of aten::to
    ghstack-source-id: 46c2a0627c60ddc58c48b47e1791159d755bbf83
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/48976

--- test/test_jit_fuser_te.py
+++ test/test_jit_fuser_te.py
@@ -1210,6 +1210,106 @@ class TestTEFuser(JitTestCase):
         else:
             return v.to(dtype)
 
+    def test_torch_to(self):
+        # test no op
+        @torch.jit.script
+        def foo(x):
+            return x.to(torch.float)
+
+        foo(torch.tensor([3.], dtype=torch.float))
+        foo(torch.tensor([3.], dtype=torch.float))
+        FileCheck().check_not("TensorExpr").run(torch.jit.last_executed_optimized_graph())
+
+        # test not fusing non-const inputs
+        @torch.jit.script
+        def foo(x, dtype: int):
+            return x.to(dtype)
+
+        foo(torch.tensor([3.], dtype=torch.float), torch.int)
+        foo(torch.tensor([3.], dtype=torch.float), torch.int)
+        FileCheck().check_not("TensorExpr").run(torch.jit.last_executed_optimized_graph())
+
+        # test not fusing to_pinned inputs
+        @torch.jit.script
+        def foo(x, dtype: int):
+            return x.to(pin_memory=True)
+
+        foo(torch.tensor([3.], dtype=torch.float), torch.int)
+        foo(torch.tensor([3.], dtype=torch.float), torch.int)
+        FileCheck().check_not("TensorExpr").run(torch.jit.last_executed_optimized_graph())
+
+
+        # test across-device not supported
+        if torch.cuda.is_available():
+            @torch.jit.script
+            def foo(x):
+                return x.to(device="cuda")
+
+            foo(torch.tensor([3.], dtype=torch.float))
+            foo(torch.tensor([3.], dtype=torch.float))
+            FileCheck().check_not("TensorExpr").run(torch.jit.last_executed_optimized_graph())
+
+        sizes = [(1, 4), (4, 4)]
+        # reuses cast impl, smaller dtype set for faster test
+        dtypes = [
+            torch.bool,
+            torch.int,
+            torch.float16,
+            torch.float32,
+            torch.float64,
+        ]
+
+        class MyMod(torch.nn.Module):
+            def __init__(self, dtype):
+                super(MyMod, self).__init__()
+                self.dtype = dtype
+
+            def forward(self, x):
+                return x.to(self.dtype)
+
+        bad_dtypes = []
+        for dtype, output_dtype, device, size in product(dtypes, dtypes, self.devices, sizes):
+            if dtype == output_dtype:
+                continue
+
+            x = self.data_for(dtype, device, size=size)
+            mod = MyMod(output_dtype)
+            ref = mod.forward(x)
+            # use freezing to make non-Tensor args to `to` constant
+            mod = torch.jit.freeze(torch.jit.script(mod.eval()))
+            warmup_forward(mod.forward, x)
+            self.assertEqual(ref, mod.forward(x))
+            self.assertLastGraphAllFused()
+
+    def test_nan_to_num(self):
+        nan_vals = torch.tensor([float('nan'), float('inf'), -float('inf'), 3.14])
+        sizes = [(1, 4), (4, 4)]
+        dtypes = [
+            torch.int,
+            torch.float16,
+            torch.float32,
+            torch.float64,
+        ]
+        for dtype, device, size in product(dtypes, self.devices, sizes):
+            # TODO
+            if dtype == torch.float16 and not LLVM_ENABLED:
+                continue
+
+            x = self.data_for(dtype, device, size=size)
+            x[0] = nan_vals.to(dtype)
+
+            def fn(x):
+                return torch.nan_to_num(x), torch.nan_to_num(x, 1., 2., 3.)
+            ref = fn(x)
+            try:
+                t = torch.jit.trace(fn, (x,))
+                self.assertEqual(ref, t(x))
+                self.assertAllFused(t.graph_for(x))
+            except Exception as e:
+                raise RuntimeError(
+                    " ".join(["Failed:", str(dtype), "torch.nan_to_num", device, str(size)])
+                )
+
     def test_masked_fill(self):
         # check scalar overload
         def foo(x, mask):
